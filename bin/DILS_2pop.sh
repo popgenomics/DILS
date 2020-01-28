71886b6b5f4c1c3065362d66a2dacb7294a4dd56
popgenomics <tauraduboudin@gmail.com>
2020-01-28 16:19:18 +0100

    add module load R

--- bin/DILS_2pop.sh
+++ bin/DILS_2pop.sh
@@ -3,4 +3,7 @@
 ## the provided argument is for --configfile, expecting the yaml file
 module load pypy/2.7-5.10.0
 module load snakemake
-snakemake --snakefile /shared/mfs/data/home/croux/softwares/DILS/bin/Snakefile_2pop -p -j 500 --configfile ${1} --cluster-config /shared/mfs/data/home/croux/softwares/DILS/bin/cluster_2pop.json --cluster "sbatch --nodes={cluster.node} --ntasks={cluster.n} --cpus-per-task={cluster.cpusPerTask} --time={cluster.time}"
+module load r/3.5.1
+binpath='/shared/mfs/data/home/croux/softwares/DILS/bin'
+snakemake --snakefile ${binpath}/Snakefile_2pop -p -j 500 --configfile ${1} --cluster-config ${binpath}/cluster_2pop.json --cluster "sbatch --nodes={cluster.node} --ntasks={cluster.n} --cpus-per-task={cluster.cpusPerTask} --time={cluster.time}"
+
