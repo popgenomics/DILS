2da9522ce0accda8a75393a43b96e0cc3bdd6af3
popgenomics <camille.roux.1983@gmail.com>
2020-05-17 15:50:23 +0200

    better specification of cluster usage

--- bin/DILS_2pop.sh
+++ bin/DILS_2pop.sh
@@ -6,5 +6,5 @@ module load snakemake
 module load r/3.6.3
 module load python/2.7
 binpath='/shared/mfs/data/home/croux/softwares/DILS/bin'
-snakemake --snakefile ${binpath}/Snakefile_2pop -p -j 500 --configfile ${1} --cluster-config ${binpath}/cluster_2pop.json --cluster "sbatch --nodes={cluster.node} --ntasks={cluster.n} --cpus-per-task={cluster.cpusPerTask} --time={cluster.time}"
+snakemake --snakefile ${binpath}/Snakefile_2pop -p -j 500 --configfile ${1} --cluster-config ${binpath}/cluster_2pop.json --cluster "sbatch --nodes={cluster.node} --ntasks={cluster.n} --cpus-per-task={cluster.cpusPerTask} --time={cluster.time} --mem-per-cpu={cluster.memPerCpu}"
 
